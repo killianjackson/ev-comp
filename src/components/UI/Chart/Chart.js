50b1a2f17528bd10e16f202d68ad3d8e3af70e55
Killian Jackson <killianjacksn@gmail.com>
2020-07-30 17:26:17 -0700

    Added animation to chart

--- src/components/UI/Chart/Chart.js
+++ src/components/UI/Chart/Chart.js
@@ -2,26 +2,33 @@ import React from 'react';
 import {VictoryChart, VictoryLine} from 'victory';
 import materialTheme from './material';
 
+import classes from './Chart.css';
+
 const chart = (props) => {
   return (
-    <VictoryChart
-      theme={materialTheme}
-    >
-      <VictoryLine
-        style={{
-          data: { stroke: "#c43a31" },
-          parent: { border: "1px solid #ccc"}
-        }}
-        data={props.lineGasData}
-      />
-      <VictoryLine
-        style={{
-          data: { stroke: "#4285F4" },
-          parent: { border: "1px solid #ccc"}
-        }}
-        data={props.lineEVData}
-      />
-    </VictoryChart>
+    <div className={classes.Chart}>
+      <VictoryChart
+        theme={materialTheme}
+        animate={{duration: 1000}}
+        height={200}
+        width={400}
+      >
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          data={props.lineGasData}
+        />
+        <VictoryLine
+          style={{
+            data: { stroke: "#4285F4" },
+            parent: { border: "1px solid #ccc"}
+          }}
+          data={props.lineEVData}
+        />
+      </VictoryChart>
+    </div>
   );
 }
 
